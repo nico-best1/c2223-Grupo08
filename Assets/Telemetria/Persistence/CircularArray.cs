a9c7a683394b6a50b59681e3507368812a048bb1
DavidRubioMoreno <darubi04@ucm.es>
2026-06-23 12:47:50 +0200

    ajuste array circular usando queue

--- Assets/Telemetria/Persistence/CircularArray.cs
+++ Assets/Telemetria/Persistence/CircularArray.cs
@@ -1,53 +1,37 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using UnityEditor.PackageManager;
+
 public class CircularArray
 {
     private const int MaxBuffer = 50;
-    private int head = 0;
-    private TrackerEvent[] events = new TrackerEvent[MaxBuffer];
-    private int eventSize = 0;
+    private Queue<TrackerEvent> queue = new Queue<TrackerEvent>(MaxBuffer);
 
     /// <summary>
-    /// AÃ±ade un evento al array para luego guardalo por persistencia.
+    /// Añade un evento a la cola para luego guardalo por persistencia.
     /// </summary>
-    /// <param name="e">Evento a aÃ±adir</param>
-    /// <returns>0 si se ha aÃ±adido. 1 si se ha sobreescrito un elemento. -1 Error</returns>
+    /// <param name="e">Evento a añadir</param>
+    /// <returns>0 si se ha añadido. 1 si se ha sobreescrito un elemento. -1 Error</returns>
     public int addEvent(TrackerEvent e)
     {
-        if(e == null)
+        if (e == null)
             return -1;
 
-        int index = (head + eventSize) % MaxBuffer;
-
-        if (eventSize == MaxBuffer)
+        int result = 0;
+        if (queue.Count == MaxBuffer)
         {
-            events[head] = e;
-
-            head = (head + 1) % MaxBuffer;
-
-            return 1;
+            // descarta el mas viejo
+            queue.Dequeue();
+            result = 1;
         }
 
-        events[index] = e;
-
-        eventSize++;
-
-        return 0;
+        queue.Enqueue(e);
+        return result;
     }
 
     public List<string> getEvents(ISerializer serializer)
     {
         List<string> serializedEvents = new List<string>();
-
-        for (int i = 0; i < eventSize; i++)
-        {
-            serializedEvents.Add(serializer.serialize(events[(i+head) % MaxBuffer]));
-        }
-        eventSize = 0;
-
+        while (queue.Count > 0)
+            serializedEvents.Add(serializer.serialize(queue.Dequeue()));
         return serializedEvents;
     }
 }
